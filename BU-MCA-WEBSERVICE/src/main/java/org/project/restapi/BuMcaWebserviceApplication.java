f49820302e19af51ab3a5de337771e07b0ae6daf
sandeepam5588 <78194280+sandeepam5588@users.noreply.github.com>
2021-03-02 19:22:44 +0530

    initial commit "all the best"

---
+++ BU-MCA-WEBSERVICE/src/main/java/org/project/restapi/BuMcaWebserviceApplication.java
@@ -0,0 +1,13 @@
+package org.project.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuMcaWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuMcaWebserviceApplication.class, args);
+	}
+
+}
---
+++ BU-MCA-WEBSERVICE/src/test/java/org/project/restapi/BuMcaWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuMcaWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
